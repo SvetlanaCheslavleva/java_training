29279d33e91346dd6a48429b21faac3a25cf36ba
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-05 18:26:49 +0300

    Задание №5

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/appmanager/UserHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/appmanager/UserHelper.java
@@ -4,40 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.trening.addressbook.model.UserData;
 
-public class UserHelper {
-  private WebDriver wd;
+public class UserHelper extends HelperBase{
+
 
   public UserHelper(WebDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToHomePage() {
-    wd.findElement(By.linkText("home page")).click();
+    click(By.linkText("home page"));
   }
 
   public void submitUserCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
   public void fillUserForm(UserData userData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(userData.getUser_firstname());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(userData.getUser_lastname());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(userData.getPhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
+    type(By.name("firstname"), userData.getUser_firstname());
+    type(By.name("lastname"), userData.getUser_lastname());
+    type(By.name("address"), userData.getAddress());
+    type(By.name("mobile"), userData.getPhone());
+    type(By.name("email"), userData.getEmail());
   }
 
   public void initUserCreation() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
