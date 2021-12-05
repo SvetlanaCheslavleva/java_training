e4677e95b1478dfcd8dc72382251631750e69662
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-05 15:16:24 +0300

    Задание №4

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/UserCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.trening.addressbook;
 
 import java.util.concurrent.TimeUnit;
+
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,35 +14,59 @@ public class UserCreationTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testUserCreation() throws Exception {
-    wd.findElement(By.linkText("add new")).click();
+    initUserCreation();
+    fillUserForm(new UserData("test_user_firstname", "test_user_lastname", "test_address", "test_phone", "test_email"));
+    submitUserCreation();
+    returnToHomePage();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitUserCreation() {
+    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void fillUserForm(UserData userData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("test_user_firstname");
+    wd.findElement(By.name("firstname")).sendKeys(userData.getUser_firstname());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("test_user_lastname");
+    wd.findElement(By.name("lastname")).sendKeys(userData.getUser_lastname());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("test_address");
+    wd.findElement(By.name("address")).sendKeys(userData.getAddress());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("test_phone");
+    wd.findElement(By.name("mobile")).sendKeys(userData.getPhone());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("test_email");
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    wd.findElement(By.linkText("home page")).click();
-    wd.findElement(By.linkText("Logout")).click();
+    wd.findElement(By.name("email")).sendKeys(userData.getEmail());
+  }
+
+  private void initUserCreation() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.stqa.trening.addressbook;
+
+public class UserData {
+  private final String user_firstname;
+  private final String user_lastname;
+  private final String address;
+  private final String phone;
+  private final String email;
+
+  public UserData(String user_firstname, String user_lastname, String address, String phone, String email) {
+    this.user_firstname = user_firstname;
+    this.user_lastname = user_lastname;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getUser_firstname() {
+    return user_firstname;
+  }
+
+  public String getUser_lastname() {
+    return user_lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
