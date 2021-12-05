dec6545a1c27aad20dc1143159e28019628695cb
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-05 19:26:43 +0300

    Создание теста для изменения группы "GroupModificationTests"

---
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.trening.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.trening.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase{
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().iniGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("Test_1", "Test_2", "Test_4"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
