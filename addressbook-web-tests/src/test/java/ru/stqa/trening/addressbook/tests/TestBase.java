be393a854554859236ddf530f6ac4cc199962c67
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-06 23:10:29 +0300

    Задание №7 + класс Equality

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.trening.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.trening.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
