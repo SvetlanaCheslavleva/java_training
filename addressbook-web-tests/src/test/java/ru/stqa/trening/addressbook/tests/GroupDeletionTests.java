6ae7d45b9986ff556ec4d4b9be5204c5e2369c5a
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-10 13:32:15 +0300

    Реалищована проверка и обсепечение выполения предисловий

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.trening.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.trening.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Test_1", "Test_2", "Test_3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase{
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("Test_1", "Test_2", "Test_3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Test_1", "Test_2", "Test_3"));
