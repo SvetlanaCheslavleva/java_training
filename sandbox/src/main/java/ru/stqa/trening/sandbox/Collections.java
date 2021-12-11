0a3db499dba1df1c204939a9ea8a4543499bde07
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-11 23:35:16 +0300

    Циклы и Коллекции, создание класса Primes, Collection и тестов для этих классов

---
+++ sandbox/src/main/java/ru/stqa/trening/sandbox/Collections.java
@@ -0,0 +1,13 @@
+package ru.stqa.trening.sandbox;
+
+public class Collections {
+
+  public static void main(String[] args){
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    for (String l: langs){
+      System.out.println("Я хочу выучить " + l);
+    }
+  }
+
+}
