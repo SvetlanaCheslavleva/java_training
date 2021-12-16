85a69e7103ae690c8ff0ecc9c0b6e4e06b877cae
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-16 10:33:07 +0300

    Изцчение циклов и коллекций на примере классов Primes and Collection

--- sandbox/src/main/java/ru/stqa/trening/sandbox/Collections.java
+++ sandbox/src/main/java/ru/stqa/trening/sandbox/Collections.java
@@ -1,11 +1,18 @@
 package ru.stqa.trening.sandbox;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
   public static void main(String[] args){
     String[] langs = {"Java", "C#", "Python", "PHP"};
 
-    for (String l: langs){
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+
+    for (String l: languages){
       System.out.println("Я хочу выучить " + l);
     }
   }
