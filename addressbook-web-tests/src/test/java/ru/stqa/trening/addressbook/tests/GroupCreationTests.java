55eaa41e5403a1122ed9f055683be5a7ed399338
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2021-12-17 20:42:59 +0300

    4.9 Лямбда выражения, вычисление максимального Id

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.trening.addressbook.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -12,8 +14,15 @@ public class GroupCreationTests extends TestBase{
     public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("Test_1", "Test_2", "Test_3"));
+    GroupData group = new GroupData("Test_1", "Test_2", "Test_3");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+    group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
   }
 }
