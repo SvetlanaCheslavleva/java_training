1bd40cbbd66c771de625324e904c7ad7819a6755
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2022-01-04 17:59:48 +0300

    Лекция 6.8: АВтоматичаское закрытие файлов после их сипользования реализовано для групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/trening/addressbook/tests/UserCreationTests.java
@@ -22,33 +22,35 @@ public class UserCreationTests extends TestBase {
   @DataProvider
   public Iterator<Object[]> validUsersFromXml() throws IOException {
     File photo = new File("src/test/resources/stru.png");
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.xml")));
-    String xml = "";
-    String line = reader.readLine();
-    while (line != null) {
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.allowTypes(new Class[]{ UserData.class });
+      xstream.processAnnotations(UserData.class);
+      List<UserData> users = (List<UserData>) xstream.fromXML(xml);
+      return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    XStream xstream = new XStream();
-    xstream.allowTypes(new Class[]{ UserData.class });
-    xstream.processAnnotations(UserData.class);
-    List<UserData> users = (List<UserData>) xstream.fromXML(xml);
-    return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
   public Iterator<Object[]> validUsersFromJson() throws IOException {
     File photo = new File("src/test/resources/stru.png");
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.json")));
-    String json = "";
-    String line = reader.readLine();
-    while (line != null) {
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson =new Gson();
+      List<UserData> users = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
+      return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson =new Gson();
-    List<UserData> users = gson.fromJson(json, new TypeToken<List<UserData>>(){}.getType());
-    return users.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validUsersFromJson")
