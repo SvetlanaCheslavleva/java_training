e6afd2d6d9475f137f0648d2f791185b6592dc46
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2022-01-13 20:14:54 +0300

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP

--- mantis-tests/src/test/java/ru/stqa/trening/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/trening/mantis/appmanager/ApplicationManager.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -44,4 +45,11 @@ public class ApplicationManager {
     wd.quit();
   }
 
+  public HttpSession newSession() {
+    return new HttpSession(this);
+  }
+
+  public String getProperty(String key) {
+    return properties.getProperty(key);
+  }
 }
