9f4895ef37e6bdd09fca620ab79febfec5ff2721
Svetlana Cheslavleva <svetlana.cheslavleva@kofax.com>
2022-01-13 22:26:49 +0300

    Исправление лекции 8.3

--- mantis-tests/src/test/java/ru/stqa/trening/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/trening/mantis/appmanager/HttpSession.java
@@ -1,5 +1,6 @@
 package ru.stqa.trening.mantis.appmanager;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -9,7 +10,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-import org.omg.CORBA.NameValuePair;
+
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -34,7 +35,7 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpClient.execute(post);
     String body = geTextForm(response);
-    return body.contains(String.format("<span class=\"italic\">&s</span>", username));
+    return body.contains(String.format("<span class=\"italic\">%s</span>", username));
   }
 
   private String geTextForm(CloseableHttpResponse response) throws IOException {
@@ -49,6 +50,6 @@ public class HttpSession {
     HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
     CloseableHttpResponse response = httpClient.execute(get);
     String body = geTextForm(response);
-    return body.contains(String.format("<span class=\"italic\">&s</span>"));
+    return body.contains(String.format("<span class=\"italic\">&s</span>, username"));
   }
 }
